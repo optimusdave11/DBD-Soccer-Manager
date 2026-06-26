f6f2085dc0918495de3e073f2222147d301b37e8
optimusdave11 <daveaisosa11@gmail.com>
2026-06-26 08:55:04 +0100

    update 1

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -3,34 +3,27 @@ import { STORES } from "./schema";
 
 export async function seedStore<T>(
   storeName: string,
-    data: T[]
-    ): Promise<void> {
-      const store = await database.getStore(storeName, "readwrite");
+  data: T[]
+): Promise<void> {
+  const store = await database.getStore(storeName, "readwrite");
 
-        for (const item of data) {
-            store.put(item);
-              }
-              }
+  await Promise.all(
+    data.map(item => store.put(item))
+  );
+}
 
-              export async function seedDatabase({
-                players,
-                  clubs,
-                    leagues,
-                      competitions,
-                        managers,
-                          nations,
-                          }: {
-                            players: unknown[];
-                              clubs: unknown[];
-                                leagues: unknown[];
-                                  competitions: unknown[];
-                                    managers: unknown[];
-                                      nations: unknown[];
-                                      }) {
-                                        await seedStore(STORES.PLAYERS, players);
-                                          await seedStore(STORES.CLUBS, clubs);
-                                            await seedStore(STORES.LEAGUES, leagues);
-                                              await seedStore(STORES.COMPETITIONS, competitions);
-                                                await seedStore(STORES.MANAGERS, managers);
-                                                  await seedStore(STORES.NATIONS, nations);
-                                                  }
+export interface SeedData {
+  players: unknown[];
+  clubs: unknown[];
+  competitions: unknown[];
+  managers: unknown[];
+  nations: unknown[];
+}
+
+export async function seedDatabase(data: SeedData): Promise<void> {
+  await seedStore(STORES.PLAYERS, data.players);
+  await seedStore(STORES.CLUBS, data.clubs);
+  await seedStore(STORES.COMPETITIONS, data.competitions);
+  await seedStore(STORES.MANAGERS, data.managers);
+  await seedStore(STORES.NATIONS, data.nations);
+}
