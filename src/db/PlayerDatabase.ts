62f6593195035e77eace0efa835779b1c9dea4bf
optimusdave11 <daveaisosa11@gmail.com>
2026-07-01 00:17:10 +0100

    update PlayerDatabase.ts

---
+++ src/db/PlayerDatabase.ts
@@ -0,0 +1,52 @@
+import type { Player } from "../models/database/Player";
+
+import { playerSeeder } from "./PlayerSeeder";
+
+export class PlayerDatabase {
+
+  private players: Player[] = [];
+
+  async load(): Promise<void> {
+
+    if (this.players.length) {
+      return;
+    }
+
+    this.players =
+      await playerSeeder.seed();
+
+  }
+
+  getAll(): Player[] {
+
+    return this.players;
+
+  }
+
+  getPlayer(
+    playerId: string
+  ): Player | undefined {
+
+    return this.players.find(
+      player =>
+        String(player.PlayerID) ===
+        playerId
+    );
+
+  }
+
+  getByClub(
+    clubId: number
+  ): Player[] {
+
+    return this.players.filter(
+      player =>
+        player.ClubID === clubId
+    );
+
+  }
+
+}
+
+export const playerDatabase =
+  new PlayerDatabase();
